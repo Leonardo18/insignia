89ceedcce2c47741486b969b380509b005d522c4
leo_d <leo_d@DESKTOP-OLN2NBA>
2016-10-18 22:53:27 -0200

    Adicionado a estrutura para criação das tarefas e criaro uma pré-view dela.

---
+++ Insignia.Model/Tarefa/Tarefa.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Insignia.Model.Tarefa
+{
+    public class Tarefa
+    {
+        public int ID { get; set; }
+
+        public int EmpresaID { get; set; }
+
+        public int UsuarioID { get; set; }
+
+        [Required(ErrorMessage = "Informe o título da tarefa")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da tarefa")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o tipo da tarefa")]
+        public string Tipo { get; set; }
+
+    }
+}
---
+++ Insignia.Painel/Controllers/TarefasController.cs
@@ -0,0 +1,18 @@
+﻿using Insignia.Model.Tarefa;
+using Insignia.Painel.Helpers.CustomAttributes;
+using System.Web.Mvc;
+
+namespace Insignia.Painel.Controllers
+{
+    public class TarefasController : Controller
+    {
+        // GET: Tarefas Adicionar       
+        [IsLogged]
+        public ActionResult Adicionar()
+        {
+            var TarefaModel = new Tarefa();
+
+            return View(TarefaModel);
+        }
+    }
+}
