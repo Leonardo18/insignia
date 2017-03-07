52636882cdc1793d1f4bf00c000d395c1ea25091
Leonardo Dutra <Leonardo Dutra@LEONARDO>
2017-03-07 11:38:03 -0300

    Criado e estruturado o projeto de Site do Insígnia

---
+++ Insignia.Site/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Optimization;
+
+namespace Insignia.Site.App_Start
+{
+    public class BundleConfig
+    {
+        /// <summary>
+        /// Bundle com arquivos usados nos cshtml
+        /// </summary>
+        /// <param name="bundles">Objeto para criar os bundles</param>
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            BundleTable.EnableOptimizations = false;
+
+            //Arquivos de css usados no index
+            bundles.Add(new StyleBundle("~/Index/styles").Include(
+                    "~/Content/css/main.css"));
+
+            //Arquivos de javascript usados no index
+            bundles.Add(new ScriptBundle("~/Index/scripts").Include(
+                    "~/Scripts/js/jquery.min.js",
+                    "~/Scripts/js/jquery.dropotron.min.js",
+                    "~/Scripts/js/skel.min.js",
+                    "~/Scripts/js/skel-viewport.min.js",
+                    "~/Scripts/js/util.js",
+                    "~/Scripts/js/main.js"));
+        }
+    }
+}
---
+++ Insignia.Site/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using Insignia.Site.App_Start;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Insignia.Site
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
