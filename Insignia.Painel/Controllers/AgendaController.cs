75acea1d5bf4c3dedb20a24a562487566d952fe0
Leonardo Dutra <Leonardo Dutra@LEONARDO>
2017-03-24 16:26:28 -0300

    Adicionado google calendar

--- Insignia.Painel/Controllers/AgendaController.cs
+++ Insignia.Painel/Controllers/AgendaController.cs
@@ -1,10 +1,21 @@
-﻿using Insignia.Painel.Helpers.CustomAttributes;
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Insignia.Painel.Helpers.CustomAttributes;
+using System;
+using System.IO;
+using System.Threading;
 using System.Web.Mvc;
 
 namespace Insignia.Painel.Controllers
 {
     public class AgendaController : Controller
     {
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Calendar";
+
         /// <summary>
         /// GET: Agenda Visualizar
         /// </summary>
@@ -22,6 +33,54 @@ namespace Insignia.Painel.Controllers
         [HttpPost, IsLogged]
         public ActionResult Visualizar(int i = 0)
         {
+            UserCredential credential;
+
+            using (var stream = new FileStream("client_secret_215187720738-qvd9a4kbm69cqd5iuutgekhspg67l8ar.apps.googleusercontent.com.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                  GoogleClientSecrets.Load(stream).Secrets,
+                  Scopes,
+                  "user",
+                  CancellationToken.None,
+                  new FileDataStore(credPath, true)
+                ).Result;
+            }
+
+            // Create Calendar Service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            //Upcoming events
+            Events events = request.Execute();
+            if (events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (string.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                }
+            }
+            else
+            {
+            }
+
             return View();
         }
     }
